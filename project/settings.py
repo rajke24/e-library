75a274f598b6e6b95386a45e1abd1eb2882ea265
rajke24 <jarek.andrzejewski99@gmail.com>
2021-02-03 15:17:44 +0100

    Add login page

--- project/settings.py
+++ project/settings.py
@@ -24,6 +24,7 @@ INSTALLED_APPS = [
 
     # my apps
     'landing',
+    'users',
 ]
 
 MIDDLEWARE = [
--- users/views.py
+++ users/views.py
@@ -1,3 +1,4 @@
 from django.shortcuts import render
 
-# Create your views here.
+def login_page(request):
+    return render(request, 'users/login.html', {'title': 'Log in'})
