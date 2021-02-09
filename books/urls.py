2604721a633b89d68f27de76fe1af3ef621f5e38
rajke24 <jarek.andrzejewski99@gmail.com>
2021-02-09 18:34:07 +0100

    Add book views and fix templates

--- books/urls.py
+++ books/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import library
+from .views import library, book_detail
 
 
 urlpatterns = [
     path('library/', library, name='library'),
+    path('book/<int:id>', book_detail, name='bookDetails'),
 ]
--- books/views.py
+++ books/views.py
@@ -1,5 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
-# Create your views here.
+from .models import Book, Genre
+
 def library(request):
-    return render(request, 'books/library.html', {'title': 'Library'})
+    bookList = Book.objects.all().order_by('title')
+    genres = Genre.objects.all().order_by('name')
+
+    context = {
+        'title': 'Library',
+        'bookList': bookList,
+        'genres': genres,
+    }
+
+    return render(request, 'books/library.html', context)
+
+def book_detail(request, id):
+    book = get_object_or_404(Book, id=id)
+
+    return render(request, 'books/book_details.html', {'book': book})
