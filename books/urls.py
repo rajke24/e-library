82e223db044d76a82a73aadec51946fb63cb8587
rajke24 <jarek.andrzejewski99@gmail.com>
2021-02-10 12:34:08 +0100

    Add searching sorting and filtering of books

--- books/urls.py
+++ books/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import library, book_detail
+from .views import library, book_detail, book_filter_view
 
 
 urlpatterns = [
     path('library/', library, name='library'),
-    path('book/<int:id>/', book_detail, name='bookDetails'),
+    path('library/book/<int:id>/', book_detail, name='bookDetails'),
+    path('library/search/', book_filter_view, name='filter_book'),
 ]
--- books/views.py
+++ books/views.py
@@ -6,12 +6,12 @@ from .forms import OpinionCreateForm
 
 
 def library(request):
-    bookList = Book.objects.all().order_by('title')
+    book_list = Book.objects.all().order_by('title')
     genres = Genre.objects.all().order_by('name')
 
     context = {
         'title': 'Library',
-        'bookList': bookList,
+        'book_list': book_list,
         'genres': genres,
     }
 
@@ -29,6 +29,13 @@ def book_detail(request, id):
             rating += opinion.rating
         book.rating = round(rating / len(opinions), 2)
         book.save()
+    
+    def add_opinion(form):
+        new_opinion = form.save(commit=False)
+        new_opinion.author = request.user
+        new_opinion.book = book
+        new_opinion.save()
+        update_book_rating()
 
     if request.method == 'POST':
         if opinions.filter(author=request.user).exists():
@@ -37,14 +44,56 @@ def book_detail(request, id):
         form = OpinionCreateForm(request.POST)
 
         if form.is_valid():
-            new_opinion = form.save(commit=False)
-            new_opinion.author = request.user
-            new_opinion.book = book
-            new_opinion.save()
-            update_book_rating()
+            add_opinion(form)
             messages.success(request, "Review succesfully added!")
             return redirect(f'/book/{id}/')
     else:
         form = OpinionCreateForm()
 
     return render(request, 'books/book_details.html', {'book': book})
+
+
+def is_valid_queryparam(param):
+    return param != '' and param is not None
+
+
+def book_filter_view(request):
+    books = Book.objects.all()
+    book_title = request.GET.get('book_title')
+    book_author = request.GET.get('book_author')
+    book_genre = request.GET.get('book_genre')
+    book_rating = request.GET.get('book_rating')
+
+    sort_method = request.GET.get('sort')
+
+    genres = Genre.objects.all().order_by('name')
+    searched = False
+    most_popular = False
+
+    if is_valid_queryparam(book_title):
+        books = books.filter(title__icontains=book_title).order_by('title')
+        searched = True
+    elif is_valid_queryparam(book_author):
+        books = books.filter(author__last_name__icontains=book_author) | books.filter(
+            author__first_name__icontains=book_author)
+        books = books.order_by('title')
+        searched = True
+    if is_valid_queryparam(book_genre):
+        books = books.filter(genre__name=book_genre)
+
+    if is_valid_queryparam(sort_method):
+        if sort_method == 'popularity':
+            books = books.order_by('-rating')
+            most_popular = True
+        else:
+            books = books.order_by(sort_method)
+
+    context = {
+        'book_list': books,
+        'genres': genres,
+        'searched': searched,
+        'genre': book_genre,
+        'popularity_ranking': most_popular,
+    }
+
+    return render(request, 'books/library.html', context)
