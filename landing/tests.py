d1d0ebd2a48b499f990ead61e129cc085739f4a5
rajke24 <jarek.andrzejewski99@gmail.com>
2021-02-11 14:20:34 +0100

    Add landing app tests

--- landing/tests.py
+++ landing/tests.py
@@ -1,3 +1,14 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.urls import reverse
 
-# Create your tests here.
+class TestHomeView(TestCase):
+
+    def setUp(self):
+        self.client = Client()
+        self.home_url = reverse('home')
+
+    def test_home_GET(self):
+        response = self.client.get(self.home_url, follow=True)
+        
+        self.assertEquals(response.status_code, 200)
+        self.assertTemplateUsed(response, 'landing/home.html')
