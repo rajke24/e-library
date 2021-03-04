a9ca4d405883a6caaea708591f871b95b7ad7392
rajke24 <jarek.andrzejewski99@gmail.com>
2021-03-04 14:05:04 +0100

    Fix commented html code error

--- project/settings.py
+++ project/settings.py
@@ -49,18 +49,13 @@ LOGGING = {
     'version': 1,
     'disable_existing_loggers': False,
     'handlers': {
-        'file': {
-            'level': 'DEBUG',
-            'class': 'logging.FileHandler',
-            'filename': './logs/debug.log',
+        'console': {
+            'class': 'logging.StreamHandler',
         },
     },
-    'loggers': {
-        'django': {
-            'handlers': ['file'],
-            'level': 'DEBUG',
-            'propagate': True,
-        },
+    'root': {
+        'handlers': ['console'],
+        'level': 'WARNING',
     },
 }
 
