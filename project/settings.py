6658bc10bc327da3df303c04d85fcbe2f2877d90
rajke24 <jarek.andrzejewski99@gmail.com>
2021-03-04 11:36:00 +0100

    Set DEBUG value

--- project/settings.py
+++ project/settings.py
@@ -9,7 +9,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('LIBRARY_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['nice-e-library.herokuapp.com/']
 
